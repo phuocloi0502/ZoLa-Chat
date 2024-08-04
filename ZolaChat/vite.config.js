85dd77073f8e75f4ecd80d47485773e8d8bce6fa
phuocloi0502 <Vophuocloi0502@gmail.com>
2024-08-04 18:49:18 +0900

    fix deploy

--- ZolaChat/vite.config.js
+++ ZolaChat/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ZoLa-Chat/", // Đường dẫn cơ sở phải khớp với tên repository của bạn
   plugins: [react()],
-})
+});
